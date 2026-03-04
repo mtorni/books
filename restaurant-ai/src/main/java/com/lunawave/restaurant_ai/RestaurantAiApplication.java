9525f2371efc442c8790bc4544292ded5f6d9a15
Michael Torni <mtorni@yahoo.com>
2026-03-04 11:51:12 -0500

    kuh

---
+++ restaurant-ai/src/main/java/com/lunawave/restaurant_ai/RestaurantAiApplication.java
@@ -0,0 +1,13 @@
+package com.lunawave.restaurant_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAiApplication.class, args);
+	}
+
+}
